e6cde412535c9b77dcb79c562fa678369bbe976c
kovalevroman7 <67242846+kovalevroman7@users.noreply.github.com>
2020-06-28 01:50:04 +0300

    Создание и вызов генератора

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "./styles.css";
 
+function* myGen() {
+  yield 1;
+  yield 2;
+}
+
+const testGenerator = () => {
+  const gen = myGen();
+  console.log(gen.next());
+  console.log(gen.next());
+  console.log(gen.next());
+};
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <button onClick={testGenerator}>Test generators</button>
     </div>
   );
 }
