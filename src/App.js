0cb6f6e26a127efded2c23623eebf35c9c92f2a5
kovalevroman7 <67242846+kovalevroman7@users.noreply.github.com>
2020-06-28 23:44:50 +0300

    yield* Позволяет включать один генератор в другой

--- src/App.js
+++ src/App.js
@@ -13,12 +13,27 @@ const testGenerator = () => {
   console.log(gen.next());
 };
 
+const testGeneratorWithStar = () => {
+  function* generatorWithStar() {
+    yield* myGen();
+    yield* [3, 4];
+  }
+
+  const testGen = generatorWithStar();
+  console.log("1 - myGen.next();", testGen.next());
+  console.log("2 - myGen.next();", testGen.next());
+  console.log("3 - myGen.next();", testGen.next());
+  console.log("4 - myGen.next();", testGen.next());
+  console.log("5 - myGen.next();", testGen.next());
+};
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <button onClick={testGenerator}>Test generators</button>
+      <button onClick={testGeneratorWithStar}>Test generator with star</button>
     </div>
   );
 }
